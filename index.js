8770ef11eb4e5286d7b8c3788dcb223e486fc2b4
Gustavo Letério <gustavoleterio@gmail.com>
2022-07-29 00:09:21 -0300

    Code refactoring and button animations

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
+const pokemonSprite = document.getElementById("pokemonSprite");
+const pokemonAmbient = document.getElementById("pokemonAmbient");
+
+const pokemonScreenId = document.getElementById("pokemonScreenId");
+const pokemonScreenName = document.getElementById("pokemonScreenName");
+
 const pokemonName = document.getElementById("pokemonName");
 const pokemonHeight = document.getElementById("pokemonHeight");
 const pokemonWeight = document.getElementById("pokemonWeight");
 const pokemonType = document.getElementById("pokemonType");
-const pokemonScreenNumber = document.getElementById("pokemonScreenNumber");
-const pokemonScreenName = document.getElementById("pokemonScreenName");
-const pokemonSprite = document.getElementById("pokemonSprite");
-const pokemonAmbient = document.getElementById("pokemonAmbient");
 
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 
 const pokedexForm = document.getElementById("pokedexForm");
+const searchButton = document.getElementById("search");
 const input = document.getElementById("args");
 
 var index = 0;
@@ -41,55 +44,100 @@ const renderInformation = async (pokemon) => {
 
   data.id == "???" ? (index = 0) : (index = data.id);
 
+  spawnPokemon(data);
+};
+
+const spawnPokemon = (data) => {
+  setFrameInfo(data);
+  setCompleteInfo(data);
+  setType(data);
+  setSprite(data);
+};
+
+const setFrameInfo = (data) => {
   pokemonScreenName.innerHTML = data.name;
-  pokemonScreenNumber.innerHTML = data.id;
+  pokemonScreenId.innerHTML = data.id;
+};
+
+const setCompleteInfo = (data) => {
   pokemonName.innerHTML = data.name;
   pokemonHeight.innerHTML = `${data.height / 10} m`;
   pokemonWeight.innerHTML = `${data.weight / 10} kg`;
+};
 
+const setType = (data) => {
   const types = data.types.map((type) => {
     return type.type.name;
   });
   pokemonType.innerHTML = `${types}`;
 
-  setAmbient(types[0]);
-  setSprite(data);
+  setAmbient(types[0], data);
 };
 
-const setAmbient = (mainType) => {
-  if (mainType == "999")
+const setAmbient = (mainType, data) => {
+  if (data.id >= 899) {
+    pokemonAmbient.src = "/assets/ambients/image-unknown.webp";
+    return;
+  }
+
+  if (mainType == 999) {
     pokemonAmbient.src = "/assets/ambients/missigno-ambient.webp";
-  if (mainType == "fire") pokemonAmbient.src = "/assets/ambients/red.webp";
+    return;
+  }
+
+  if (mainType == "fire") {
+    pokemonAmbient.src = "/assets/ambients/red.webp";
+    return;
+  }
+
   if (
     mainType == "bug" ||
     mainType == "grass" ||
     mainType == "normal" ||
     mainType == "poison"
-  )
+  ) {
     pokemonAmbient.src = "/assets/ambients/green.webp";
-  if (mainType == "dark" || mainType == "ghost")
+    return;
+  }
+
+  if (mainType == "dark" || mainType == "ghost") {
     pokemonAmbient.src = "/assets/ambients/dark.webp";
-  if (mainType == "ice" || mainType == "water")
+    return;
+  }
+
+  if (mainType == "ice" || mainType == "water") {
     pokemonAmbient.src = "/assets/ambients/blue.webp";
+    return;
+  }
+
   if (
     mainType == "fighting" ||
     mainType == "ground" ||
     mainType == "rock" ||
     mainType == "steel"
-  )
+  ) {
     pokemonAmbient.src = "/assets/ambients/yellow.webp";
+    return;
+  }
+
   if (
     mainType == "dragon" ||
     mainType == "fairy" ||
     mainType == "flying" ||
     mainType == "psychic"
-  )
+  ) {
     pokemonAmbient.src = "/assets/ambients/purple.webp";
-  if (mainType == "electric")
+    return;
+  }
+
+  if (mainType == "electric") {
     pokemonAmbient.src = "/assets/ambients/darkblue.webp";
+    return;
+  }
 };
 
 const setSprite = (data) => {
+  //Missigno Sprite
   if (data.id == "???") {
     pokemonSprite.src = "/assets/missigno-sprite.webp";
     pokemonSprite.style.opacity = `1`;
@@ -97,7 +145,7 @@ const setSprite = (data) => {
     return;
   }
 
-  //Bug correction, API doesn't contain the correct url when researching with the number
+  //Bug correction, API doesn't contain the correct url when researching with the number 376 - METAGROSS
   if (data.id == 376) {
     pokemonSprite.src =
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-v/black-white/animated/376.gif";
@@ -115,63 +163,70 @@ const setSprite = (data) => {
   const secondarySrc =
     data["sprites"]["versions"]["generation-v"]["black-white"]["front_default"];
 
-  if (primarySrc) {
-    pokemonSprite.src =
-      data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
-        "front_default"
-      ];
-    pokemonSprite.style.opacity = 1;
-  } else if (secondarySrc) {
-    pokemonSprite.src =
-      data["sprites"]["versions"]["generation-v"]["black-white"][
-        "front_default"
-      ];
-    pokemonSprite.style.opacity = 1;
-  } else {
-    pokemonAmbient.src = "/assets/ambients/image-unknown.webp";
+  if (data.id >= 899) {
     pokemonSprite.style.opacity = 0;
+    return;
   }
+
+  pokemonSprite.style.opacity = 1;
+  pokemonSprite.src = primarySrc || secondarySrc;
 };
 
 const spriteSize = (data) => {
-  let sizeVariable;
+  let sizeVariable = 0;
+  if (data.id >= 650) {
+    sizeVariable += 3;
+    pokemonSprite.style.top = "57%";
+  }
+
   if (data.height / 10 >= 3) {
-    sizeVariable = 20;
+    sizeVariable += 20;
     pokemonSprite.style.width = `${sizeVariable}%`;
   }
 
   if (data.height / 10 >= 1.7 && data.height / 10 < 3) {
-    sizeVariable = 15;
-    if (data.id >= 650) sizeVariable += 3;
+    sizeVariable += 15;
     pokemonSprite.style.width = `${sizeVariable}%`;
   }
 
   if (data.height / 10 >= 1 && data.height / 10 < 1.7) {
-    sizeVariable = 12;
-    if (data.id >= 650) sizeVariable += 3;
+    sizeVariable += 12;
     pokemonSprite.style.width = `${sizeVariable}%`;
   }
 
   if (data.height / 10 < 1) {
-    sizeVariable = 10;
-    if (data.id >= 650) sizeVariable += 3;
+    sizeVariable += 10;
     pokemonSprite.style.width = `${sizeVariable}%`;
   }
-
-  if (data.id >= 650) pokemonSprite.style.top = "57%";
 };
 
 pokedexForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  searchButton.classList.add("clicked");
+  setTimeout(() => {
+    searchButton.classList.remove("clicked");
+  }, 100);
+
   renderInformation(input.value.toLowerCase() || "0");
   input.value = "";
 });
 
-nextButton.addEventListener("click", (e) => {
-  renderInformation(index + 1);
+previousButton.addEventListener("click", () => {
+  previousButton.classList.add("clicked");
+  setTimeout(() => {
+    previousButton.classList.remove("clicked");
+  }, 100);
+
+  index = index || 906;
+  renderInformation(index - 1);
 });
 
-previousButton.addEventListener("click", (e) => {
-  index == 0 ? (index = 906) : "";
-  renderInformation(index - 1);
+nextButton.addEventListener("click", () => {
+  nextButton.classList.add("clicked");
+  setTimeout(() => {
+    nextButton.classList.remove("clicked");
+  }, 100);
+
+  renderInformation(index + 1);
 });
